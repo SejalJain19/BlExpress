451b3f63c4fd6293eba36b044ba447bc5524e515
SejalJain19 <jain.19.sejal@gmail.com>
2021-06-11 17:35:22 +0530

    first commit

---
+++ Patholabs_ExpressAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using Patholabs_ExpressAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Patholabs_ExpressAPI.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        private Patholabs_ExpressEntities db = new Patholabs_ExpressEntities();
+        [HttpPost]
+        public IHttpActionResult Registration(User_Admin reg)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User_Admin.Add(reg);
+                int rows=db.SaveChanges();
+                if (rows == 1)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+               
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Patholabs_ExpressAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Patholabs_ExpressAPI.WebApiApplication" Language="C#" %>
